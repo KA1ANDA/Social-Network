9c08c66f2bba00c63c5f3cdcad98e77b5272be45
KA1ANDA <tokokalanda20@gmail.com>
2023-06-12 00:10:48 +0400

    added styles for all chat

--- src/Components/ChatUi/ChatMessage.js
+++ src/Components/ChatUi/ChatMessage.js
@@ -9,13 +9,15 @@ const ChatMessage = ({photo , userName ,message}) => {
 
   return (
     <div className={styles.chatMessage}>
-      <div>
-      <img src={photo} />
+      <div className={styles.photoNameWrapper}>
+        <div className={styles.photo}>
+          <img src={photo} />
+        </div>
+        <div className={styles.name}>
+          <h2>{userName}</h2>
+        </div>
       </div>
-      <div>
-        <h2>{userName}</h2>
-      </div>
-      <div>
+      <div className={styles.message}>
         <h2>{message}</h2>
       </div>
     </div>
