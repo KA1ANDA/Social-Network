e07f7b2783e192317ded157c64ae4ad844208a22
KA1ANDA <tokokalanda20@gmail.com>
2023-07-08 00:02:42 +0400

    newbranch

--- proxy.js
+++ proxy.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({origin: 'ka1anda.github.io'}));
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello from the proxy server!');
