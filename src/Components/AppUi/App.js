f2f9a714db71d2b62ff8cd2eba2ea9df85f9acc1
KA1ANDA <tokokalanda20@gmail.com>
2023-09-08 19:50:07 +0400

    changing styles adding collapsed menu

--- src/Components/AppUi/App.js
+++ src/Components/AppUi/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProfileNav from '../NavUi/Nav';
 import UserInfo from '../UserHeaderInfoUi/UserHeaderInfo';
 import Main from '../MainUi/Main';
 import styles from './app.module.scss'
 import { useSelector } from 'react-redux';
 import AboutMeEdit from '../ProfileUi/AboutMeUi/AboutMeEdit';
+import {RxHamburgerMenu} from 'react-icons/rx'
+import {IoIosArrowBack} from 'react-icons/io'
+
 
 
 
@@ -13,14 +16,60 @@ const App = () => {
   const {editMode} = useSelector(state => state.profileSlice)
 
 
+  const [isLargeScreen, setIsLargeScreen] = useState(false);
+  const [colapsedMenu, setColapsedMenu] = useState(false);
+
+
+  useEffect(() => {
+
+    const checkScreenSize = () => {
+      setIsLargeScreen(window.innerWidth <= 768);
+    };
+
+  
+    checkScreenSize();
+
+   
+    window.addEventListener('resize', checkScreenSize);
+
+    return () => {
+      window.removeEventListener('resize', checkScreenSize);
+    };
+  }, []);
+
+
+  const toggleColapsedMenu = () =>{
+    setColapsedMenu(!colapsedMenu)
+  }
+
   return (
     <div className={`${styles.app} ${editMode && styles.diableScroll}`}>
-      <div className={styles.header}>
-        <UserInfo />
-      </div>
-      <div className={styles.nav}>
-        <ProfileNav />
-      </div>
+      {isLargeScreen && colapsedMenu &&
+      <>
+        <div className={styles.blur}> </div>
+        <div className={styles.colapsedMenu}>
+          <div className={styles.closeMenu} onClick={toggleColapsedMenu}><IoIosArrowBack/></div>
+          <UserInfo />
+          <ProfileNav />
+        </div>
+      </>
+      
+      }
+
+
+
+      {isLargeScreen ? 
+      <div className={styles.showMoreIcon} onClick={toggleColapsedMenu}> <RxHamburgerMenu/> </div>
+      :
+      <>
+        <div className={styles.header}>
+          <UserInfo />
+        </div>
+        <div className={styles.nav}>
+          <ProfileNav />
+        </div>
+      </>
+      }
       <div className={styles.main}>
         <Main />
       </div>
